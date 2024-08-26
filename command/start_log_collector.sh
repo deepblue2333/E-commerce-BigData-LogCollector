02acf3038847dcd0d13af3bd2967316c66d6b74f
deepblue2333 <125679300+deepblue2333@users.noreply.github.com>
2024-08-26 23:17:18 +0800

    Update start_log_collector.sh

--- command/start_log_collector.sh
+++ command/start_log_collector.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
 FLUME_HOME="/root/lib_link/flume-ng"
-ssh cdh02 "$FLUME_HOME/flume-ng/bin/flume-ng agent --conf-file $FLUME_HOME/conf/file-flume-kafka.properties --name a1
-          --conf $FLUME_HOME/conf -Dflume.root.logger=INFO,console > /opt/module/flume/log1.txt 2>&1 &"
+ssh cdh02 ". ~/.bash_profile; $FLUME_HOME/bin/flume-ng agent --conf-file $FLUME_HOME/conf/file-flume-kafka.properties --name a1 --conf $FLUME_HOME/conf -Dflume.root.logger=INFO,console > /opt/module/flume/log1.txt 2>&1 &"
 
-ssh cdh03 "$FLUME_HOME/flume-ng/bin/flume-ng agent --conf-file $FLUME_HOME/conf/kafka-flume-hdfs.properties --name a1
-          --conf $FLUME_HOME/conf -Dflume.root.logger=INFO,console > /opt/module/flume/log1.txt 2>&1 &"
+ssh cdh03 ". ~/.bash_profile; $FLUME_HOME/bin/flume-ng agent --conf-file $FLUME_HOME/conf/kafka-flume-hdfs.properties --name a1 --conf $FLUME_HOME/conf -Dflume.root.logger=INFO,console > /opt/module/flume/log1.txt 2>&1 &"
 
 print "Start Done"
