8002c4eb5c9df9f1cb17de7b898b75ec2087339c
Zhou Chengyu <zhouchengyu@datagrand.com>
2024-08-26 22:49:38 +0800

    add start/stop sh

---
+++ command/start_log_collector.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+FLUME_HOME="/root/lib_link/flume-ng"
+ssh cdh02 "$FLUME_HOME/flume-ng/bin/flume-ng agent --conf-file $FLUME_HOME/conf/file-flume-kafka.properties --name a1
+          --conf $FLUME_HOME/conf -Dflume.root.logger=INFO,console > /opt/module/flume/log1.txt 2>&1 &"
+
+ssh cdh03 "$FLUME_HOME/flume-ng/bin/flume-ng agent --conf-file $FLUME_HOME/conf/kafka-flume-hdfs.properties --name a1
+          --conf $FLUME_HOME/conf -Dflume.root.logger=INFO,console > /opt/module/flume/log1.txt 2>&1 &"
+
+print "Start Done"
