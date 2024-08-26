d72e04d2037b593cbdf28cab604db81eb009fba7
Zhou Chengyu <zhouchengyu@datagrand.com>
2024-08-26 23:22:46 +0800

    debug

--- command/start_log_collector.sh
+++ command/start_log_collector.sh
@@ -7,4 +7,4 @@ ssh cdh02 "$FLUME_HOME/flume-ng/bin/flume-ng agent --conf-file $FLUME_HOME/conf/
 ssh cdh03 "$FLUME_HOME/flume-ng/bin/flume-ng agent --conf-file $FLUME_HOME/conf/kafka-flume-hdfs.properties --name a1
           --conf $FLUME_HOME/conf -Dflume.root.logger=INFO,console > /opt/module/flume/log1.txt 2>&1 &"
 
-print "Start Done"
+echo "Start Done"
