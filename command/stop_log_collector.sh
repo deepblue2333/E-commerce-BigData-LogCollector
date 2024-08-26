22295886190b052cf78f4151f9399e8953b197c1
Zhou Chengyu <zhouchengyu@datagrand.com>
2024-08-26 23:23:14 +0800

    debug

--- command/stop_log_collector.sh
+++ command/stop_log_collector.sh
@@ -2,6 +2,6 @@
 
 ssh cdh02 "ps -ef | grep file-flume-kafka | grep -v grep| awk '{print $2}'| xargs -n1 kill -9"
 
-ssh cdh03 "ps -ef | gp kafka-flume-hdfs | grep -v grep| awk '{print $2}'| xargs -n1 kill -9"
+ssh cdh03 "ps -ef | grep kafka-flume-hdfs | grep -v grep| awk '{print $2}'| xargs -n1 kill -9"
 
 echo "Stop Done"
